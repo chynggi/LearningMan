4e1502ad8da305b97fb32b4bd5f9c431cc3b7a9d
vision <vision@iot-2>
2019-11-19 12:47:32 +0900

    게시판진행중

--- TeamProjPHP/DataAnalystics/DataAnalysticsBoardList.php
+++ TeamProjPHP/DataAnalystics/DataAnalysticsBoardList.php
@@ -1,14 +1,40 @@
 <!DOCTYPE html>
 <html>
+
 <head>
+
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <title>데이터 분석 게시판 리스트</title>
+
+<link href="./css/bootstrap.css" rel='stylesheet' type='text/css'>
+<link href="./css/boost.css" rel='stylesheet' type='text/css'>
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <link rel="stylesheet" href="./css/bootstrap.css">
 <script type="text/javascript" src="./js/bootstrap.js"></script>
+
+<style type="text/css">
+.jumbotron {
+	background-image: url('./DataAnalysticsImg/jumbotronBackground.jpg');
+	background-size: cover;
+	text-shadow: black 0.5em 0.5em 0.5em;
+	color: black;
+}
+
+.img-button1 {
+        background: url( "./DataAnalysticsImg/글쓰기.png" ) no-repeat;
+        border: none;
+        width: 32px;
+        height: 32px;
+        cursor: pointer;
+      }
+</style>
+
 </head>
 
 <body>
-	<h1 class="display-4">board_list.php</h1>
+        <table class="table table-bordered" border="1" align = "center" style="width:60%;">
         <?php
         $currentPage = 1;
         
@@ -17,15 +43,7 @@
         }
 
         // mysqli_connect()함수로 커넥션 객체 생성
-        $conn = mysqli_connect("localhost", "root", "", "test");
-
-        // 커넥션 객체 생성 확인
-        if ($conn) {
-            echo "연결 성공<br>";
-        } 
-        else {
-            die("연결 실패 : " . mysqli_error());
-        }
+        $conn = mysqli_connect("localhost", "root", "", "test");           
 
         // 페이징 작업을 위한 테이블 내 전체 행 갯수 조회 쿼리
         $sqlCount = "SELECT count(*) FROM board";
@@ -33,86 +51,109 @@
         
         if ($rowCount = mysqli_fetch_array($resultCount)) {
             $totalRowNum = $rowCount["count(*)"]; // php는 지역 변수를 밖에서 사용 가능.
-        }
+        }       
 
-        // 행 갯수 조회 쿼리가 실행 됐는지 여부
-        if ($resultCount) {
-            echo "행 갯수 조회 성공 : " . $totalRowNum . "<br>";
-        } 
-        else {
-            echo "결과 없음: " . mysqli_error($conn);
-        }
-
-        $rowPerPage = 20; // 페이지당 보여줄 게시물 행의 수
-        
+        $rowPerPage = 10; // 페이지당 보여줄 게시물 행의 수        
         $begin = ($currentPage - 1) * $rowPerPage;
 
         // board 테이블을 조회해서 board_no, board_title, board_user, board_date 필드 값을 내림차순으로 정렬하여 모두 가져 오는 쿼리
         // 입력된 begin값과 rowPerPage 값에 따라 가져오는 행의 시작과 갯수가 달라지는 쿼리
         $sql = "SELECT board_no, board_title, board_user, board_date FROM board order by board_no desc limit " . $begin . "," . $rowPerPage . "";
-
-        // 1페이지 limit 0,5 만약 10페이지 limit 45,5
-        $result = mysqli_query($conn, $sql);
-
-        // 쿼리 조회 결과가 있는지 확인
-        if ($result) {
-            echo "조회 성공";
-        } 
-        else {
-            echo "결과 없음: " . mysqli_error($conn);
-        }
+        $result = mysqli_query($conn, $sql);              
         ?>
-
-        <table class="table table-bordered" border="1">
-		
+        		
 		<tr>
-			<td>번호(board_no)</td>
-			<td>제목(board_title)</td>
-			<td>작성자(board_user)</td>
-			<td>작성일(board_date)</td>
-			<td>수정(board_update_form.php)</td>
-			<td>삭제(board_delete_form.php)</td>
+			<td align = "center" bgcolor = "#3e5baa" style="width:7%;"><font color = "white">번호</font></td>
+			<td align = "center" bgcolor = "#3e5baa" style="width:53%;"><font color = "white">제목</font></td>
+			<td align = "center" bgcolor = "#3e5baa" style="width:15%;"><font color = "white">작성자</font></td>
+			<td align = "center" bgcolor = "#3e5baa" style="width:11%;"><font color = "white">작성일</font></td>
+			<td align = "center" bgcolor = "#3e5baa" style="width:7%;"><font color = "white">수정</font></td>
+			<td align = "center" bgcolor = "#3e5baa" style="width:7%;"><font color = "white">삭제</font></td>
 		</tr>
 
-            <?php
-            
+            <?php            
             // 반복문을 이용하여 result 변수에 담긴 값을 row변수에
             // 계속 담아서 row변수의 값을 테이블에 출력한다.
             while ($row = mysqli_fetch_array($result)) {
                 ?>
                 <tr>
-					<td>
+					<td align = "center" bgcolor = "#e6ebfa">
                         <?php
                         echo $row["board_no"];
                         ?>
                    </td>
 					<td>
     					<?php
-                        echo "<a href='./board_detail.php?board_no=" . $row["board_no"] . "'>";
+                        echo "<a href='./boardDetail.php?board_no=" . $row["board_no"] . "'>";
                         echo $row["board_title"];
                         echo "</a>";
                         ?>
                     </td>
-					<td>
+					<td align = "center">
                         <?php
                         echo $row["board_user"];
                         ?>
                     </td>
-					<td>
+					<td align = "center">
                         <?php
                         echo $row["board_date"];
                         ?>
-                    </td>
+                    </td >
 						<?php
-                        echo "<td><a href='./board_update_form.php?board_no=" . $row["board_no"] . "'>수정</a></td>";
-                        echo "<td><a href='./board_delete_form.php?board_no=" . $row["board_no"] . "'>삭제</a></td>";
+                        echo "<td align = 'center'><a class='btn btn-primary2' href='./boardUpdate.php?board_no=" . $row["board_no"] . "'>수정</a></td>";
+                        echo "<td align = 'center'><a class='btn btn-primary2' href='./boardDelete.php?board_no=" . $row["board_no"] . "'>삭제</a></td>";
                         ?>
-                	</tr>
+                	</tr>            		
             <?php
             } 
             // 와일루프 종료
             ?>
+			<tr>
+            	<td bgcolor = "#e6ebfa">         		
+            		<a class="btn btn-primary2" href="./DataAnalysticsBoardAdd.php" >
+  					<img src="./DataAnalysticsImg/글쓰기.png" width="30" height="30" title = "글쓰기" alt="글쓰기 로고">
+  					</a>            		
+            	</td>
+            	<td align = "center" colspan = "20">
+            		<?php
+            			echo "<a class='btn btn-primary2' href ='./DataAnalysticsBoardList.php?currentPage=" . ($currentPage - 1) . "'>이전 페이지</a>&nbsp;&nbsp;";           		    
+            			echo "<a class='btn btn-primary2' href='./DataAnalysticsBoardList.php?currentPage=" . ($currentPage + 1) . "'>다음 페이지</a>&nbsp;&nbsp;";
+            		?>
+            	</td>           	
+            </tr>
+            <?php 
+            echo ("<tr>");
+            echo ("<td align = 'center' colspan = '6'>");
 
+            if ($conn) {
+                echo "연결 성공<br>";
+            }
+            else {
+                die("연결 실패 : " . mysqli_error());
+            }
+
+            if ($rowCount = mysqli_fetch_array($resultCount)) {
+                
+            }
+            
+            if ($resultCount) {
+                echo "행 갯수 조회 성공 : " . $totalRowNum . "<br>";
+            }
+            else {
+                echo "결과 없음: " . mysqli_error($conn);
+            }
+
+            if ($result) {
+                echo "조회 성공";
+                
+            }
+            else {
+                echo "결과 없음: " . mysqli_error($conn);
+            }
+            
+            echo ("</td>");
+            echo ("</tr>");
+            ?>
         </table>
         
         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
@@ -124,7 +165,7 @@
             
             // 이전 버튼이 클릭될때 GET방식으로 currentPage변수 값에
             // 1을 뺀 값이 넘어가도록 함
-            echo "<a class='btn btn-primary2' href ='./board_list.php?currentPage=" . ($currentPage - 1) . "'>이전</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
+         //   echo "<a class='btn btn-primary2' href ='./board_list.php?currentPage=" . ($currentPage - 1) . "'>이전</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
         }
         
         $lastPage = ($totalRowNum - 1) / $rowPerPage;
@@ -139,27 +180,11 @@
 
             // 다음 버튼이 클릭될때 GET방식으로 currentPage변수 값에
             // 1을 더한 값이 넘어가도록 함
-            echo "<a class='btn btn-primary2' href='./board_list.php?currentPage=" . ($currentPage + 1) . "'>다음</a>";
+          //  echo "<a class='btn btn-primary2' href='./board_list.php?currentPage=" . ($currentPage + 1) . "'>다음</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
         }
-
-        mysqli_close($conn); // 중요: 디비서버의 메모리를 효율적으로 운영
-        
-        ?>
-
-        &nbsp;&nbsp;&nbsp;
-        
-        <a class="btn btn-primary" href="./board_add_form.php">글 쓰기</a>
-	
+        mysqli_close($conn); // 중요: 디비서버의 메모리를 효율적으로 운영        
+        ?>	
 	<br>
-	
-	<br>
-	
-	<br>
-	
-	<br>
-	
-	<br>
-
 </body>
 
 </html>
