a8ed254cb1a58bb55043a5a47805906196a7f938
ssy <ssy@iot-6>
2019-11-20 14:59:01 +0900

    1.1.0

---
+++ TeamProjJava/target/classes/common/MBUtils.java
@@ -0,0 +1,29 @@
+package common;
+
+import java.io.IOException;
+import java.io.InputStream;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+public class MBUtils {
+	
+	public static SqlSessionFactory getSqlSessionFactory() {
+		String resource = "common/SqlMapConfig.xml";
+
+		InputStream  reader;
+		try {
+			reader = Resources.getResourceAsStream(resource);
+		}catch (IOException e) {
+
+			throw new IllegalArgumentException(e);
+		}
+		return new SqlSessionFactoryBuilder().build(reader);
+	}
+	
+	public static SqlSession getSession() {
+		return getSqlSessionFactory().openSession(false);
+	}
+	
+}
---
+++ TeamProjJava/target/classes/dao/IDAO.java
@@ -0,0 +1,13 @@
+package dao;
+
+import java.util.List;
+
+public interface IDAO<T, K> {
+	//public boolean isExist(K key); 
+	public List<T> selectAll();
+	public void insert(T vo);
+	public void update(T vo);
+	public int delete(K key);
+	public T selectById(K key);
+	public long maxIdNum();
+}
