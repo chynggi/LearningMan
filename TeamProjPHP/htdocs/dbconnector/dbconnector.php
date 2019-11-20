7b98d60a43a3c3632bff39f0ec8bed9753bb097f
최용규 <chynggi69@gmail.com>
2019-11-20 12:41:53 +0900

    JSP Database Mapper Set

---
+++ TeamProjPHP/htdocs/dbconnector/dbconnector.php
@@ -0,0 +1,3 @@
+<?php $tns = " (DESCRIPTION = (ADDRESS_LIST = (ADDRESS = (PROTOCOL = TCP)(HOST = 192.168.0.18)(PORT = 1521)) ) (CONNECT_DATA = (SERVICE_NAME = xe) ) ) "; try { $conn = new PDO("oci:dbname=".$tns.";charset=utf8", "team", "team"); } catch (PDOException $e) { echo "Failed to obtain database handle " . $e->getMessage(); } ?>
+
+OK?
