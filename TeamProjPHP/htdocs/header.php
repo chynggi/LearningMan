04bdaf725fd5bdd3d4f512df78aac4e7c617d85a
EYH <EYH@iot-2>
2019-11-28 14:01:57 +0900

    PHP 헤더 수정

--- TeamProjPHP/htdocs/header.php
+++ TeamProjPHP/htdocs/header.php
@@ -20,6 +20,13 @@
 
   <!-- Custom styles for this template -->
   <link href="css/clean-blog.css" rel="stylesheet">
+  
+  <!-- 상단바 메뉴 -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href='https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+  <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+  <link href="css/clean-blog.min.css" rel="stylesheet">
 
 </head>
 
@@ -38,17 +45,57 @@
 			</button>
 			<div class="collapse navbar-collapse" id="navbarResponsive">
 				<ul class="navbar-nav ml-auto">
-					<li class="nav-item"><a class="nav-link" href="index">About</a>
+					<li class="nav-item"><a class="nav-link" href="#">Home</a>
 					</li>
-					<li class="nav-item"><a class="nav-link" href="OOP_Board/oop_B_index.php">OOP</a></li>
-					<li class="nav-item"><a class="nav-link" href="#">DBMS</a>
-					</li>
-					<li class="nav-item"><a class="nav-link" href="#">Server
-							Side</a></li>
-					<li class="nav-item"><a class="nav-link" href="#">Frame
-							Works</a></li>
-					<li class="nav-item"><a class="nav-link" href="DataAnalysticsMain.php">Data
-							Analystics</a></li>
+					
+					<li class="nav-item dropdown dmenu" >
+      					<a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+     	 				Server
+							Side
+      					</a>
+      					<div class="dropdown-menu sm-menu">
+        					<a class="dropdown-item" href="OOP_Board/oop_B_index.php">OOP란?</a>
+        					<a class="dropdown-item" href="OOP_Board/oop_B_index.php">질문 게시판</a>     			
+      					</div>
+     	  			</li>
+					<li class="nav-item dropdown dmenu" >
+      					<a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+     	 				Server
+							Side
+      					</a>
+      					<div class="dropdown-menu sm-menu">
+        					<a class="dropdown-item" href="#">DBMS란?</a>
+        					<a class="dropdown-item" href="#">질문 게시판</a>     			
+      					</div>
+     	  			</li>
+					<li class="nav-item dropdown dmenu" >
+      					<a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+     	 				Server
+							Side
+      					</a>
+      					<div class="dropdown-menu sm-menu">
+        					<a class="dropdown-item" href="#">서버 사이드란?</a>
+        					<a class="dropdown-item" href="#">질문 게시판</a>     			
+      					</div>
+     	  			</li>
+					<li class="nav-item dropdown dmenu" >
+      					<a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+     	 				Frame Works
+      					</a>
+      					<div class="dropdown-menu sm-menu">
+        					<a class="dropdown-item" href="#">프레임 워크란?</a>
+        					<a class="dropdown-item" href="#">질문 게시판</a>     			
+      					</div>
+     	  			</li>
+					<li class="nav-item dropdown dmenu" >
+      					<a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+     	 				Data Analystics
+      					</a>
+      					<div class="dropdown-menu sm-menu">
+        					<a class="dropdown-item" href="DataAnalysticsMain.php">데이터 분석이란?</a>
+        					<a class="dropdown-item" href="DataAnalysticsBoardList.php">질문 게시판</a>     			
+      					</div>
+     	  			</li>
 				</ul>
 			</div>
 		</div>
@@ -73,8 +120,4 @@
 			</div>
 		</div>
 	</header>
-
-
-
-
 </html>
