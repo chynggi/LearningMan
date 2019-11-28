8d55b72f069f9b0f7688840ab863f0a6ce692261
EYH <EYH@iot-2>
2019-11-28 14:40:21 +0900

    PHP 헤더 수정

--- TeamProjPHP/htdocs/header.php
+++ TeamProjPHP/htdocs/header.php
@@ -1,123 +1,67 @@
-<?php ?>
 <!DOCTYPE html>
 <html lang="ko">
-
 <head>
-
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="description" content="">
   <meta name="author" content="">
-
-
-  <!-- Bootstrap core CSS -->
-  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
-  <!-- Custom fonts for this template -->
-  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-  <link href='https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
-  <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
-
-  <!-- Custom styles for this template -->
-  <link href="css/clean-blog.css" rel="stylesheet">
   
-  <!-- 상단바 메뉴 -->
+  <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+  <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <link href="./css/bootstrap.css" rel='stylesheet' type='text/css'>
+  <link href="./css/boost.css" rel='stylesheet' type='text/css'>
+
   <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
   <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
   <link href='https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
   <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
-  <link href="css/clean-blog.min.css" rel="stylesheet">
-
+  <link href="css/clean-blog.css" rel="stylesheet">
+    
+  <style type="text/css">
+    .jumbotron {
+	background-image: url('./images/jumbotronBackground.jpg');
+	background-size: cover;
+	text-shadow: black 0.5em 0.5em 0.5em;
+	color: black;
+    }
+  </style>
 </head>
-
 <body>
-
-  <!-- Navigation -->
-	<nav class="navbar navbar-expand-lg navbar-light fixed-top"
-		id="mainNav">
-		<div class="container">
-			<a class="navbar-brand" href="index.php">Learning Man</a>
-			<button class="navbar-toggler navbar-toggler-right" type="button"
-				data-toggle="collapse" data-target="#navbarResponsive"
-				aria-controls="navbarResponsive" aria-expanded="false"
-				aria-label="Toggle navigation">
-				Menu <i class="fas fa-bars"></i>
-			</button>
-			<div class="collapse navbar-collapse" id="navbarResponsive">
-				<ul class="navbar-nav ml-auto">
-					<li class="nav-item"><a class="nav-link" href="#">Home</a>
-					</li>
-					
-					<li class="nav-item dropdown dmenu" >
-      					<a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
-     	 				Server
-							Side
-      					</a>
-      					<div class="dropdown-menu sm-menu">
-        					<a class="dropdown-item" href="OOP_Board/oop_B_index.php">OOP란?</a>
-        					<a class="dropdown-item" href="OOP_Board/oop_B_index.php">질문 게시판</a>     			
-      					</div>
-     	  			</li>
-					<li class="nav-item dropdown dmenu" >
-      					<a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
-     	 				Server
-							Side
-      					</a>
-      					<div class="dropdown-menu sm-menu">
-        					<a class="dropdown-item" href="#">DBMS란?</a>
-        					<a class="dropdown-item" href="#">질문 게시판</a>     			
-      					</div>
-     	  			</li>
-					<li class="nav-item dropdown dmenu" >
-      					<a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
-     	 				Server
-							Side
-      					</a>
-      					<div class="dropdown-menu sm-menu">
-        					<a class="dropdown-item" href="#">서버 사이드란?</a>
-        					<a class="dropdown-item" href="#">질문 게시판</a>     			
-      					</div>
-     	  			</li>
-					<li class="nav-item dropdown dmenu" >
-      					<a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
-     	 				Frame Works
-      					</a>
-      					<div class="dropdown-menu sm-menu">
-        					<a class="dropdown-item" href="#">프레임 워크란?</a>
-        					<a class="dropdown-item" href="#">질문 게시판</a>     			
-      					</div>
-     	  			</li>
-					<li class="nav-item dropdown dmenu" >
-      					<a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
-     	 				Data Analystics
-      					</a>
-      					<div class="dropdown-menu sm-menu">
-        					<a class="dropdown-item" href="DataAnalysticsMain.php">데이터 분석이란?</a>
-        					<a class="dropdown-item" href="DataAnalysticsBoardList.php">질문 게시판</a>     			
-      					</div>
-     	  			</li>
-				</ul>
-			</div>
-		</div>
-	</nav>
+<nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+    <div id="head_div"></div>
+    <div class="container">
+      <a class="navbar-brand" href="index.html">Learning Man</a>
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        Menu
+        <i class="fas fa-bars"></i>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>
+          <li class="nav-item"><a class="nav-link" href="OOP_Board/oop_B_index.php">OOP</a></li>
+          <li class="nav-item"><a class="nav-link" href="#">DBMS</a></li>
+          <li class="nav-item"><a class="nav-link" href="#">서버 사이드</a></li>
+          <li class="nav-item"><a class="nav-link" href="#">프레임 워크</a></li>
+          <li class="nav-item"><a class="nav-link" href="DataAnalysticsMain.php">데이터 분석</a></li>									
+        </ul>
+      </div>
+    </div>
+  </nav>
 
   <!-- Page Header -->
-  <header class="masthead"
-		style="background-image: url('img/home-bg.jpg')">
-		<div class="overlay"></div>
-		<div class="container">
-			<div class="row">
-				<div class="col-lg-8 col-md-10 mx-auto">
-					<div class="site-heading">
-						<span class="subheading">Team Project</span>
-						<h1>Learning Man</h1>
-						<p class="M_btn">
-							<a class="col-lg-8 col-md-10 mx-auto" href="#">로그인</a> <a
-								class="col-lg-8 col-md-10 mx-auto" href="#">회원가입</a>
-						</p>
-					</div>
-				</div>
-			</div>
-		</div>
-	</header>
+  <header class="masthead" style="background-image: url('img/home-bg.jpg')">
+    <div class="overlay"></div>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 col-md-10 mx-auto">
+          <div class="page-heading">
+            <h1>Learning Man Team Project</h1>
+            <span class="subheading">made by Learning Man</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+</body>
 </html>
