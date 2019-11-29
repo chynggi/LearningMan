86039cec3b32165336d213b494e8e4fe6b32f32c
LeeDongTak <LeeDongTak@iot-11>
2019-11-29 11:15:59 +0900

    1.30

--- TeamProjJava/src/dto/Buser.java
+++ TeamProjJava/src/dto/Buser.java
@@ -2,25 +2,15 @@ package dto;
 
 public class Buser {
 	private String id;
-	private String name;
 	private String password;
+	private String name;
+	private int phone;
+	private String date;
 	public String getId() {
 		return id;
 	}
 	public void setId(String id) {
 		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public Buser(String id, String name, String password) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.password = password;
-	}
-	public void setName(String name) {
-		this.name = name;
 	}
 	public String getPassword() {
 		return password;
@@ -28,13 +18,30 @@ public class Buser {
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPhone() {
+		return phone;
+	}
+	public void setPhone(int phone) {
+		this.phone = phone;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
 	@Override
 	public String toString() {
-		return "Buser [id=" + id + ", name=" + name + ", password=" + password + "]";
+		return "Buser [id=" + id + ", password=" + password + ", name=" + name + ", phone=" + phone + ", date=" + date
+				+ "]";
 	}
-	public Buser() {
-		super();
-	}
+	
 	
 	
 	
