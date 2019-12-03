8e7c51726f184a84baa7676c026c9331f907702e
chynggi <47375675+chynggi@users.noreply.github.com>
2019-12-03 14:30:36 +0900

    Add files via upload

--- TeamProjPHP/htdocs/static/BoardInsertAction.php
+++ TeamProjPHP/htdocs/static/BoardInsertAction.php
@@ -6,16 +6,11 @@
     <body>
         <h1>데이터 분석 게시판 입력</h1>
         <?php
-        $dbname = $_POST["dbname"];
         require 'BoardDAOFunction.php';
+        $dbname = $_POST["dbname"];
         insert($dbname);
             //헤더함수를 이용하여 리스트 페이지로 리다이렉션
-        if($dbname == "OOPBOARD"){
-            //header("Location: ../OOP_Board/oop_B_index.php");
-            echo "dddd";
-        }elseif ($dbname == "SSBOARD"){
-            header("Location: ../ServerSide/ServerSideBoardList.php");
-        }
+            //header("Location: ./ServerSideBoardList.php");
         ?>
     </body>
 </html>
