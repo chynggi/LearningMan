28ad540e52d44583e64cd159834b8d3a5bb41cf4
EYH <EYH@iot-2>
2019-12-03 10:22:32 +0900

    20191203

--- TeamProjPHP/htdocs/dbconnector/dbconnector.php
+++ TeamProjPHP/htdocs/dbconnector/dbconnector.php
@@ -1,5 +1,5 @@
 <?php 
-$tns = " (DESCRIPTION = (ADDRESS_LIST = (ADDRESS = (PROTOCOL = TCP) (HOST = 192.168.0.2)(PORT = 1521)) ) (CONNECT_DATA = (SERVICE_NAME = xe) ) ) "; 
+$tns = " (DESCRIPTION = (ADDRESS_LIST = (ADDRESS = (PROTOCOL = TCP) (HOST = 192.168.0.9)(PORT = 1521)) ) (CONNECT_DATA = (SERVICE_NAME = xe) ) ) "; 
 try { 
     $conn = new PDO("oci:dbname=".$tns.";charset=utf8", "team", "team"); 
 } catch (PDOException $e) { 
