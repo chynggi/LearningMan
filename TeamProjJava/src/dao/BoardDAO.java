f4050c4a753eba5f1418a3c5716d619fdd6aaec9
최용규 <chynggi69@gmail.com>
2019-12-03 15:03:03 +0900

    Final Commit

--- TeamProjJava/src/dao/BoardDAO.java
+++ TeamProjJava/src/dao/BoardDAO.java
@@ -1,8 +1,16 @@
 package dao;
 
+import java.util.HashMap;
+import java.util.List;
+
 import dto.Board;
 
 public interface BoardDAO extends IDAO<Board, Long> {
 	public long countofcontents();
 	public Board info(long no);
+	public int update(HashMap<String, Object> para);
+	public int delete(HashMap<String, Object> para);
+	public Board info(HashMap<String, Object> para);
+	public int insert(HashMap<String, Object> para);
+	public List<Board> selectAll(String tablename);
 }
