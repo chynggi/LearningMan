61a32aa3a0e6cdaa2ba534dc4b40d80990448927
ssy <ssy@iot-6>
2019-12-04 15:29:29 +0900

    dddd

--- TeamProjPHP/htdocs/index.php
+++ TeamProjPHP/htdocs/index.php
@@ -3,7 +3,6 @@
 
 <head>
   <title>Learning Man</title>
-
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="description" content="">
@@ -14,10 +13,7 @@
   <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
   <link href="./css/bootstrap.css" rel='stylesheet' type='text/css'>
   <link href="./css/boost.css" rel='stylesheet' type='text/css'>
-
-
   <title>Learning Man</title>
-
   <!-- Bootstrap core CSS -->
   <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
   <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
@@ -26,97 +22,13 @@
   <link href="css/clean-blog.css" rel="stylesheet">
   <link href="css/login.css" rel="stylesheet">
   <link href="css/member.css" rel="stylesheet">
-
   <script>
 	$(document).ready(function() {			
 		$('#head_div').load('header.php');
 	});
   </script>
-
 </head>
-
 <body>
-
-  <!-- Navigation -->
-  <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
-    <div class="container">
-      <a class="navbar-brand" href="index.php">Learning Man</a>
-      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-        Menu
-        <i class="fas fa-bars"></i>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarResponsive">
-        <ul class="navbar-nav ml-auto">
-          <li class="nav-item">
-            <a class="nav-link" href="index.php">About</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="OOP_Board/oop_B_index.php">OOP</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="post.html">DBMS</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="contact.html">Server Side</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="contact.html">Frame Works</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="contact.html">Data Analystics</a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
-
-  <!-- Page Header -->
-  <header class="masthead" style="background-image: url('img/home-bg.jpg')">
-    <div class="overlay"></div>
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-8 col-md-10 mx-auto">
-          <div class="site-heading">
-            <span class="subheading">Team Project</span>
-            <h1>Learning Man</h1>
-            <?php
-            session_start();
-            if(!isset($_SESSION['id'])){
-            ?>
-            <p class="M_btn">        	
-            	<a class="col-lg-8 col-md-10 mx-auto" href="./login/login.php">로그인</a>
-            	<a class="col-lg-8 col-md-10 mx-auto" href="./member/member.php">회원가입</a>            	
-            </p>
-            <?php     
-            }else {
-                $user_name = $_SESSION['name'];
-            ?>
-            <p class="M_btn">
-            	<a class="col-lg-8 col-md-10 mx-auto"><strong><?php echo "$user_name"; ?></strong>님 환영합니다.</a>
-            	<a class="col-lg-8 col-md-10 mx-auto" href="./member/member_update_form.php">본인정보 수정</a>           	
-            	<a class="col-lg-8 col-md-10 mx-auto" href="./login/logout.php">로그아웃</a>           	
-            </p>
-            <?php
-            }
-            ?>
-          </div>
-        </div>
-      </div>
-    </div>
-  </header>
-	<style>
-		.M_btn>a {
-			text-decoration: none;
-			color:#fff;
-		}
-        .M_btn>p {
-			text-decoration: none;
-			color:#fff;
-		}
-		.M_btn>a:hover {
-			color:#000;
-		}
-	</style>
   <div id="head_div"></div>
   <!-- Main Content -->
   <div class="container">
@@ -228,21 +140,42 @@
     <div class="container">
       <div class="row">
         <div class="col-lg-8 col-md-10 mx-auto">
-    
+          <ul class="list-inline text-center">
+            <li class="list-inline-item">
+              <a href="#">
+                <span class="fa-stack fa-lg">
+                  <i class="fas fa-circle fa-stack-2x"></i>
+                  <i class="fab fa-twitter fa-stack-1x fa-inverse"></i>
+                </span>
+              </a>
+            </li>
+            <li class="list-inline-item">
+              <a href="#">
+                <span class="fa-stack fa-lg">
+                  <i class="fas fa-circle fa-stack-2x"></i>
+                  <i class="fab fa-facebook-f fa-stack-1x fa-inverse"></i>
+                </span>
+              </a>
+            </li>
+            <li class="list-inline-item">
+              <a href="#">
+                <span class="fa-stack fa-lg">
+                  <i class="fas fa-circle fa-stack-2x"></i>
+                  <i class="fab fa-github fa-stack-1x fa-inverse"></i>
+                </span>
+              </a>
+            </li>
+          </ul>
           <p class="copyright text-muted">Copyright &copy; Your Website 2019</p>
         </div>
       </div>
     </div>
   </footer>
-
   <!-- Bootstrap core JavaScript -->
   <script src="vendor/jquery/jquery.min.js" type="text/javascript" charset="utf-8"></script>
   <script src="vendor/bootstrap/js/bootstrap.bundle.min.js" type="text/javascript" charset="utf-8"></script>
-
   <!-- Custom scripts for this template -->
   <script src="js/clean-blog.min.js" type="text/javascript" charset="utf-8"></script>
   <script src="js/contents_any.js" type="text/javascript" charset="utf-8"></script>
-
 </body>
-
 </html>
