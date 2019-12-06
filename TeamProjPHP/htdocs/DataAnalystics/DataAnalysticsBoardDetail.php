b560c2f4acbe0df45c7d6be0854f60431f74e70c
EYH <EYH@iot-2>
2019-12-06 14:32:46 +0900

    PHP 게시판 수정/삭제버튼 표시 수정

--- TeamProjPHP/htdocs/DataAnalystics/DataAnalysticsBoardDetail.php
+++ TeamProjPHP/htdocs/DataAnalystics/DataAnalysticsBoardDetail.php
@@ -80,13 +80,16 @@
             </table>  		
   			<br>
   			
-  		<input type="hidden" name = "board_id" value=" <?=$_SESSION["id"]?> ">
+  		<input type="hidden" name = "board_id" value=" <?=$oneRow["ID"]?> ">
 		<input type="hidden" name = "board_no" value="<?php echo $oneRow["NO"]?>">
 		<input type="hidden" name = "dbname" value="DABOARD">
+		
+		<?php if(isset($_SESSION["id"]) && $_SESSION["id"] == $oneRow["ID"]){ ?>
   		<a class="btn btn-primary2" href="./DataAnalysticsBoardUpdate.php?board_no=<?=$oneRow["NO"]?>">수정</a>
 		&nbsp;&nbsp;&nbsp;
 		<a class="btn btn-primary2" href="./DataAnalysticsBoardDelete.php?board_no=<?=$oneRow["NO"]?>">삭제</a>
 		&nbsp;&nbsp;&nbsp;
+		<?php } ?>
 		<a class="btn btn-primary2" href="./DataAnalysticsBoardList.php">리스트로돌아가기</a>	
 
  		
